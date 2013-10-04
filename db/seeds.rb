55342864f25fd648c0746d0139d7fe49c3a47a31
dbc-apprentice <dbc@dbc.com>
2013-10-04 16:16:45 -0700

    seed file fixed, list created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require 'faker'
 require_relative '../app/models/task'
 
+List.create!(name: 'bullshit')
+
 20.times do
-  p Task.new(complete: 0, list_id: 1, text: Faker::Company.bs)
+  Task.create!(complete: 0, list_id: 1, text: Faker::Company.bs)
 end
