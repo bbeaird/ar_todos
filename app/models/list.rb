de7cc06797f4d91b96dc7afe104e9dd8ed166460
bbeaird <bbeaird@gmail.com>
2013-10-04 17:08:30 -0700

    confusion

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,14 +1,26 @@
 require_relative '../../config/application'
 
+# class List < ActiveRecord::Base
+#   has_many :tasks
+#   # def list
+#   #   self.each do |task|
+#   #     puts task
+#   #   end
+#   # end
+# end
+
+
+# test_list2 = List.new
+# p test_list2.class
 
 class List < ActiveRecord::Base
   has_many :tasks
-  def list
-    self.each do |task|
-      puts task
-    end
+    
+  def lister
+    self.tasks
   end
 end
-
-test_list = List.new
-p test_list.methods.sort
+          
+test_list = List.new(id: 1)
+t2 = Task.new(list_id: 1)
+puts test_list.tasks.map {|task| task.text}
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require 'faker'
 require_relative '../app/models/task'
 
-20.times do
-  Task.create!(list_id: 1, text: Faker::Company.bs)
-end
+List.create!(name: 'first_todo_list')
+
+# 20.times do
+#   Task.create!(list_id: 1, text: Faker::Company.bs)
+# end
