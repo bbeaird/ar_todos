76adaf09c34448ec65dc6284e9afc8489c026cdd
bbeaird <bbeaird@gmail.com>
2013-10-04 17:16:56 -0700

    what are we doing:

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,18 +1,5 @@
 require_relative '../../config/application'
 
-# class List < ActiveRecord::Base
-#   has_many :tasks
-#   # def list
-#   #   self.each do |task|
-#   #     puts task
-#   #   end
-#   # end
-# end
-
-
-# test_list2 = List.new
-# p test_list2.class
-
 class List < ActiveRecord::Base
   has_many :tasks
     
@@ -22,6 +9,6 @@ class List < ActiveRecord::Base
 end
 
           
-test_list = List.new(id: 1)
-t2 = Task.new(list_id: 1)
-puts test_list.tasks.map {|task| task.text}
+# test_list = List.new(id: 1)
+# t2 = Task.new(list_id: 1)
+# puts test_list.tasks.map {|task| task.text}
